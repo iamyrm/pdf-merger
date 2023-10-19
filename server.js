08f90b3bf4e5319b5f10b1cb8e7815a7ab0b654d
Yagyaraj Majhi <majhiyagya123@gmail.com>
2023-10-19 17:25:58 +0545

    This application can merge two pdfs to a single one

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const express = require("express");
 const path = require("path");
+const multer = require("multer");
+const { pdfMerger } = require("./merger");
+const upload = multer({ dest: "uploads/" });
 const app = express();
+
+app.use("/static", express.static("public"));
+
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.post("/merge", upload.array("pdfs", 2), async function (req, res, next) {
+  //   console.log(req.files);
+  let d = await pdfMerger(
+    path.join(__dirname, req.files[0].path),
+    (__dirname, req.files[1].path)
+  );
+  //   res.send({ data: req.files });
+
+  res.redirect(`http://localhost:3000/static/${d}.pdf`);
+  // req.files is array of `photos` files
+  // req.body will contain the text fields, if there were any
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 });
 
-// For CSS 
-app.use(express.static('public'));
+// For CSS
+app.use(express.static("public"));
